b66a621141e0f5966940f3e09c7743c30a8817e0
Sarah Lima <sarahpereiradal@gmail.com>
2026-03-05 12:22:26 -0300

    d006

---
+++ desafios/d006.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+dobro = num * 2
+triplo = num * 3
+raizquadrada = num**(1/2)
+print(f'O dobro de {num} vale {dobro}.\n O triplo de {num} vale {triplo}.\n A raiz quadrada de {num} é igual a {raizquadrada}.')
