b2bf52b5d7b685cd0e97034c6b63644865f1f9ae
Sarah Lima <sarahpereiradal@gmail.com>
2026-03-05 12:55:45 -0300

    d013

---
+++ desafios/d013.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual o salário do funcionário? R$'))
+novosalario = salario * 1.15
+print(f'Um funcionário que ganhava R${salario}, com 15% de aumento, passa a receber R${novosalario}')
