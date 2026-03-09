589af189d86ded1f3ad35f278320e062fac73eab
Sarah Lima <sarahpereiradal@gmail.com>
2026-03-09 16:43:08 -0300

    d031

---
+++ desafios/d031.py
@@ -0,0 +1,7 @@
+distancia = int(input('Qual é a distância da sua viagem? '))
+print(f'Você está prestes a começar uma viagem de {distancia}Km.')
+if distancia <= 200:
+    preco = distancia * 0.5
+else:
+    preco = distancia * 0.45
+print(f'E o preço da sua passagem será de R${preco}')
